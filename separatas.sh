bdb30a3daa8bab67ca7252cf6f45a02a2a226b7d
MariaT-13 <majotorrico13@gmail.com>
2025-06-24 21:28:11 -0300

    24-06-2025

--- separatas.sh
+++ separatas.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pdftk ./pdf/pdf-20250505TORRICOMARIA.pdf cat 2-193 output ./docs/indices-pdf-20250505TORRICOMARIA.pdf
+pdftk ./pdf/pdf-20250505TORRICOMARIA.pdf cat 2-195 output ./docs/indices-pdf-20250505TORRICOMARIA.pdf
